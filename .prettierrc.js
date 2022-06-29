3ecb89568c3266e0cc75fbe93b3e7011dcc9f9bb
Mario Acero <marioaceroc@icloud.com>
2022-06-29 17:21:17 -0300

    Config utils: tdconfig, eslint, babel and prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  importOrder: ['^src/(.*)$', '^[./]', '^[../]'],
+  importOrderSeparation: true,
 };
